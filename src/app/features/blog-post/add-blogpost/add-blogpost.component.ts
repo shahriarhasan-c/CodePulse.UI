8b5107958d3f9a3e77ccc018f9529ed8e33e3f0e
Shahriar Hasan <sh.shahriar5@gmail.com>
2024-02-10 22:30:29 +0600

    Add blogpost done

--- src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AddBlogPost } from '../models/add-blog-post.model';
+import { BlogPostService } from '../services/blog-post.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-blogpost',
   templateUrl: './add-blogpost.component.html',
   styleUrls: ['./add-blogpost.component.css']
 })
-export class AddBlogpostComponent {
+export class AddBlogpostComponent implements OnInit, OnDestroy {
   model: AddBlogPost;
+  blogpostSubscription ?: Subscription;
 
-  constructor(){
+  constructor(private services : BlogPostService,
+    private router: Router){
     this.model = {
       title: '',
       shortDescription: '',
@@ -21,8 +26,24 @@ export class AddBlogpostComponent {
       publishedDate: new Date()
     }
   }
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
   onFormSubmit(){
-    console.log(this.model);
+    this.blogpostSubscription = this.services.createBlogPost(this.model)
+    .subscribe(
+    {
+      next: (response) => {
+        this.router.navigateByUrl('/admin/blogposts');
+      }
+    }
+    )
+   
+  }
+
+  ngOnDestroy(): void {
+    this.blogpostSubscription ?.unsubscribe();
   }
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    apiBaseUrl: 'https://localhost:7287'
+    apiBaseUrl: 'https://localhost:7287',
 };
