956d20113f22d91e52e0ccbc1f166174f393a70b
Shahriar Hasan <sh.shahriar5@gmail.com>
2024-05-20 23:22:52 +0600

    Update Blogpost UI done

--- src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -1,6 +1,11 @@
+import { CategoryService } from './../../category/services/category.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { BlogPostService } from '../services/blog-post.service';
+import { BlogPost } from '../models/blog-post.model';
+import { Category } from '../../category/models/CategoryList.model';
+import { UpdateBlogPost } from '../models/update-blog-post.model';
 
 @Component({
   selector: 'app-edit-blogpost',
@@ -10,19 +15,65 @@ import { Subscription } from 'rxjs';
 export class EditBlogpostComponent implements OnInit, OnDestroy {
   id: string | null = null;
   routeSubscription ?: Subscription;
+  model?: BlogPost;
+  categories$ ?: Observable<Category[]>; 
+  selectedCategories ?: string[]; 
+  updateBlogPostSubscription ?: Subscription;
+  getBlogPostSubscription?: Subscription;
 
-  constructor(private route:ActivatedRoute){
+  constructor(private route:ActivatedRoute,
+    private blogPostService: BlogPostService,
+    private CategoryService: CategoryService, 
+    private router:Router
+  ){
 
   }
   ngOnDestroy(): void {
     this.routeSubscription?.unsubscribe();
+    this.updateBlogPostSubscription?.unsubscribe();
+    this.getBlogPostSubscription?.unsubscribe();
   }
 
   ngOnInit(): void {
+
+   this.categories$ = this.CategoryService.getAllCategories();
+
     this.routeSubscription = this.route.paramMap.subscribe({
       next: (params) => {
        this.id = params.get('id');
+       if(this.id){
+        this.getBlogPostSubscription = this.blogPostService.getBlogPostById(this.id).subscribe({
+          next:(res)=>{
+              this.model = res;
+              this.selectedCategories = res.categories.map(x => x.id);
+          }
+        })
+       }
+       
       }
     })
+  }
+
+  onFormSubmit(): void {
+    if(this.model && this.id){
+      var updateBlogPost : UpdateBlogPost ={
+        author: this.model.author,
+        content: this.model.content,
+        shortDescription: this.model.shortDescription,
+        featuredImageUrl: this.model.featuredImageUrl,
+        isVisible: this.model.isVisible,
+        publishedDate: this.model.publishedDate,
+        title: this.model.title,
+        urlHandle: this.model.urlHandle,
+        categories: this.selectedCategories ?? []
+      }
+
+     this.updateBlogPostSubscription = this.blogPostService.updateBlogPost(this.id, updateBlogPost)
+      .subscribe({
+        next:(rs)=>{
+          this.router.navigateByUrl('/admin/blogposts')
+        }
+      });
+    }
   }
 }
--- src/app/features/blog-post/services/blog-post.service.ts
+++ src/app/features/blog-post/services/blog-post.service.ts
@@ -4,6 +4,7 @@ import { AddBlogPost } from '../models/add-blog-post.model';
 import { Observable } from 'rxjs';
 import { BlogPost } from '../models/blog-post.model';
 import { environment } from 'src/environments/environment.development';
+import { UpdateBlogPost } from '../models/update-blog-post.model';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,14 @@ export class BlogPostService {
   
   getAllBlogPosts() : Observable<BlogPost[]>{
     return this.http.get<BlogPost[]>(`${environment.apiBaseUrl}/api/BlogPost`);
+  }
+
+  getBlogPostById(id: string) : Observable<BlogPost>{
+    debugger;
+    return this.http.get<BlogPost>(`${environment.apiBaseUrl}/api/Blogpost/${id}`);
+  }
+
+  updateBlogPost(id: string, updatedBlogPost: UpdateBlogPost) : Observable<BlogPost>{
+    return this.http.put<BlogPost>(`${environment.apiBaseUrl}/api/BlogPost/${id}`,updatedBlogPost);
   }
 }
